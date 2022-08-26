8488af2f6aa0a8a8d9ccf506a1c9341729067df2
Mantis <barabanovpv95@gmail.com>
2022-08-26 16:18:33 +0300

    feat 1: add method to combine linked list values into new one

---
+++ combine_linked_lists.py
@@ -0,0 +1,21 @@
+from linked_list import LinkedList, Node
+
+
+def combine_linked_lists(l_list1: LinkedList, l_list2: LinkedList):
+    # check if linked lists have same length, or it's empty lists
+    if (not l_list1.len() == l_list2.len()) or (l_list1.len() == 0):
+        return None
+
+    node1 = l_list1.head
+    node2 = l_list2.head
+    res_llist = LinkedList()
+
+    while node1 is not None:
+        # adding values for new list
+        new_value = node1.value + node2.value
+        res_llist.add_in_tail(Node(new_value))
+
+        node1 = node1.next
+        node2 = node2.next
+
+    return res_llist
