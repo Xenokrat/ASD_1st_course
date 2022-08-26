b369b70b3f7791dd32ec209eedd1e461890c7bae
Mantis <barabanovpv95@gmail.com>
2022-08-26 18:11:25 +0300

    fix 1: add more test for self.head self.tail updations

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -52,16 +52,17 @@ class MyTestCase(unittest.TestCase):
         many_list = construct_many_elements_list()
         many_list.delete(8)
         self.assertEqual(many_list.find(8), None)
+        self.assertEqual(many_list.head.value, 1)
+        self.assertEqual(many_list.tail.value, 1)
 
-        many_list = construct_many_elements_list()
         many_list.delete(1)
         self.assertEqual(many_list.find(1), many_list.tail)
         self.assertEqual(many_list.head.value, 2)
+        self.assertEqual(many_list.tail.value, 1)
 
-        many_list = construct_many_elements_list()
-        many_list.delete(1, all=True)
-        self.assertEqual(many_list.head.value, 2)
-        self.assertEqual(many_list.tail.value, 8)
+        many_list.delete(2, all=True)
+        self.assertEqual(many_list.head.value, 4)
+        self.assertEqual(many_list.tail.value, 1)
 
     def test_clean(self):
         one_list = construct_one_element_list()
@@ -95,22 +96,37 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(many_list.len(), 6)
 
     def test_insert(self):
-        s_list = LinkedList()
         n1 = Node(1)
-        n2 = Node(2)
+        n2 = Node(10)
         n3 = Node(2)
         n4 = Node(4)
         n5 = Node(5)
-        s_list.add_in_tail(n1)
-        s_list.add_in_tail(n2)
-        s_list.add_in_tail(n3)
-        s_list.add_in_tail(n4)
 
-        s_list.insert(n1, n5)
-        self.assertEqual(s_list.find(5), n5)
+        e_list = LinkedList()
 
-        s_list.insert(None, n5)
-        self.assertEqual(s_list.head, n5)
+        # insert in empty list
+        e_list.insert(None, n5)
+        self.assertEqual(e_list.head, n5)
+        self.assertEqual(e_list.tail, n5)
+
+        # insert as first element
+        e_list.insert(None, n1)
+        self.assertEqual(e_list.head, n1)
+        self.assertEqual(e_list.tail, n5)
+
+        # insert as last element
+        e_list.insert(n5, n3)
+        self.assertEqual(e_list.head, n1)
+        self.assertEqual(e_list.tail, n3)
+
+        # insert in a middle of list
+        e_list.insert(n5, n4)
+        self.assertEqual(e_list.head, n1)
+        self.assertEqual(e_list.tail, n3)
+
+        e_list.insert(n5, n2)
+        self.assertEqual(e_list.head, n1)
+        self.assertEqual(e_list.tail, n3)
 
     def test_combine_linked_lists(self):
         list1 = construct_many_elements_list()
@@ -118,6 +134,11 @@ class MyTestCase(unittest.TestCase):
         res = combine_linked_lists(list1, list2)
         self.assertEqual(res.head.value, 2)
         self.assertEqual(res.tail.value, 2)
+
+        list1 = LinkedList()
+        list2 = LinkedList()
+        res = combine_linked_lists(list1, list2)
+        self.assertEqual(res, None)
 
 
 if __name__ == '__main__':
