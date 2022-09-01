b72f914fa92570f7852d56632a3b7dba9cd83d6f
Mantis <barabanovpv95@gmail.com>
2022-09-01 11:22:14 +0300

    feat 4: add tests for postfix evaluation

--- tests/test_postfix_stack.py
+++ tests/test_postfix_stack.py
@@ -1,8 +1,16 @@
 import unittest
+from postfix_stack import eval_postfix_expression
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestPostfix(unittest.TestCase):
+    def test_postfix(self):
+        res1 = eval_postfix_expression('1 2 + 3 * =')
+        res2 = eval_postfix_expression('8 2 + 5 * 9 + =')
+        res3 = eval_postfix_expression('11 3 * 5 + 2 * =')
+        self.assertEqual(res1, 9)
+        self.assertEqual(res2, 59)
+        self.assertEqual(res3, 76)
+
 
 if __name__ == '__main__':
     unittest.main()
