53f5ef58978fff8329c63af0c68ae4be9b0e3bf7
Mantis <barabanovpv95@gmail.com>
2022-09-02 10:03:42 +0300

    feat 5: add tests for QueueStacked

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,6 +1,7 @@
 import unittest
 from queue import Queue
 from queue_rotate import rotate_queue
+from queue_2_stacks import QueueStacked
 
 
 class TestQueue(unittest.TestCase):
@@ -29,6 +30,21 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(qu.dequeue(), 4)
         rotate_queue(qu, 8)
         self.assertEqual(qu.dequeue(), 3)
+
+    def test_queue_stacked(self):
+        qus = QueueStacked()
+        self.assertEqual(qus.size(), 0)
+        qus.enqueue(1)
+        qus.enqueue(2)
+        qus.enqueue(3)
+        self.assertEqual(qus.size(), 3)
+        pop1 = qus.dequeue()
+        self.assertEqual(pop1, 1)
+        pop2 = qus.dequeue()
+        self.assertEqual(pop2, 2)
+        qus.dequeue()
+        qus.dequeue()
+        self.assertEqual(qus.size(), 0)
 
 
 if __name__ == '__main__':
