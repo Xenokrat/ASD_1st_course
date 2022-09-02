0195a5f73709abe19d67123e709fba5e96feccdb
Mantis <barabanovpv95@gmail.com>
2022-09-02 08:21:49 +0300

    feat 5: add tests for Queue

---
+++ tests/test_queue.py
@@ -0,0 +1,23 @@
+import unittest
+from queue import Queue
+
+
+class TestQueue(unittest.TestCase):
+    def test_queue(self):
+        qu = Queue()
+        self.assertEqual(qu.size(), 0)
+        qu.enqueue(1)
+        qu.enqueue(2)
+        qu.enqueue(3)
+        self.assertEqual(qu.size(), 3)
+        pop1 = qu.dequeue()
+        self.assertEqual(pop1, 1)
+        pop2 = qu.dequeue()
+        self.assertEqual(pop2, 2)
+        qu.dequeue()
+        qu.dequeue()
+        self.assertEqual(qu.size(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
