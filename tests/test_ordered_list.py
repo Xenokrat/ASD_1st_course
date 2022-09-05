782fbbee384c1f31c2213f6f5c3aa3457928b0d7
Mantis <barabanovpv95@gmail.com>
2022-09-05 20:30:08 +0300

    feat 7: add tests for OrderedList

--- tests/test_ordered_list.py
+++ tests/test_ordered_list.py
@@ -2,11 +2,81 @@ import unittest
 
 from ordered_list import OrderedList, OrderedStringList
 
+def make_test_list() -> OrderedList:
+    o_list = OrderedList(asc=True)
+    for i in (1, 3, 8, 7, 2):
+        o_list.add(i)
+    return o_list
+
+def make_test_list_str() -> OrderedStringList:
+    o_list = OrderedStringList(asc=True)
+    for i in ('a', 'b ', ' z', 'm', 'y'):
+        o_list.add(i)
+    return o_list
+
 
 class TestOrderedList(unittest.TestCase):
-    def test_ordered_list(self):
-        o_list = OrderedList(asc=True)
-        o_list.add(1)
-        o_list.add(3)
-        o_list.add(2)
-        o_list.add(8)
+    def test_is_ordered(self) -> None:
+        o_list = make_test_list()
+        self.assertEqual(o_list.head.value, 1)
+        self.assertEqual(o_list.tail.value, 8)
+        
+        self.assertEqual(o_list.head.next.value, 2)
+        self.assertEqual(o_list.head.next.next.value, 3)
+        self.assertEqual(o_list.tail.prev.value, 7)
+        self.assertEqual(o_list.tail.prev.prev.value, 3)
+
+
+    def test_find(self) -> None:
+        o_list = make_test_list()
+
+        for i in ('a', 'b ', ' z', 'm', 'y'):
+            self.assertIsNotNone(o_list.find(i))
+
+        for i in (0, 4, 5, 6):
+           self.assertIsNone(o_list.find(i))
+
+    def test_delete(self) -> None:
+        o_list = make_test_list()
+        len_ = 5
+
+        for i in (1, 3, 8, 7, 2):
+            o_list.delete(i)
+            len_ -= 1
+            self.assertEqual(o_list.len(), len_)
+
+        self.assertIsNone(o_list.head)
+        self.assertIsNone(o_list.tail)
+
+
+    def test_is_ordered_str(self) -> None:
+        o_list = make_test_list_str()
+        self.assertEqual(o_list.head.value, 'a')
+        self.assertEqual(o_list.tail.value, ' z')
+        
+        self.assertEqual(o_list.head.next.value, 'b ')
+        self.assertEqual(o_list.head.next.next.value, 'm')
+        self.assertEqual(o_list.tail.prev.value, 'y')
+        self.assertEqual(o_list.tail.prev.prev.value, 'm')
+
+
+    def test_find_str(self) -> None:
+        o_list = make_test_list_str()
+
+        for i in ('a', 'b ', ' z', 'm', 'y'):
+            self.assertIsNotNone(o_list.find(i))
+
+        for i in ('c', 'd', 'q', 't'):
+           self.assertIsNone(o_list.find(i))
+
+    def test_delete_str(self) -> None:
+        o_list = make_test_list_str()
+        len_ = 5
+
+        for i in ('a', 'b', 'z', 'm', 'y'):
+            o_list.delete(i)
+            len_ -= 1
+            self.assertEqual(o_list.len(), len_)
+
+        self.assertIsNone(o_list.head)
+        self.assertIsNone(o_list.tail)
