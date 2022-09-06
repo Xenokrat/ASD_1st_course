21bc8360ecd67e43ab2b23893fa3d202fb1cdfc0
Mantis <barabanovpv95@gmail.com>
2022-09-06 16:08:57 +0300

    feat 7: add tests for methods on empty list

--- tests/test_ordered_list.py
+++ tests/test_ordered_list.py
@@ -2,11 +2,13 @@ import unittest
 
 from ordered_list import OrderedList, OrderedStringList
 
+
 def make_test_list() -> OrderedList:
     o_list = OrderedList(asc=True)
     for i in (1, 3, 8, 7, 2):
         o_list.add(i)
     return o_list
+
 
 def make_test_list_rev() -> OrderedList:
     o_list = OrderedList(asc=False)
@@ -14,11 +16,13 @@ def make_test_list_rev() -> OrderedList:
         o_list.add(i)
     return o_list
 
+
 def make_test_list_str() -> OrderedStringList:
     o_list = OrderedStringList(asc=True)
     for i in ('a', 'b ', ' z', 'm', 'y'):
         o_list.add(i)
     return o_list
+
 
 def make_test_list_str_rev() -> OrderedStringList:
     o_list = OrderedStringList(asc=False)
@@ -26,12 +30,13 @@ def make_test_list_str_rev() -> OrderedStringList:
         o_list.add(i)
     return o_list
 
+
 class TestOrderedList(unittest.TestCase):
     def test_is_ordered(self) -> None:
         o_list = make_test_list()
         self.assertEqual(o_list.head.value, 1)
         self.assertEqual(o_list.tail.value, 8)
-        
+
         self.assertEqual(o_list.head.next.value, 2)
         self.assertEqual(o_list.head.next.next.value, 3)
         self.assertEqual(o_list.tail.prev.value, 7)
@@ -41,13 +46,12 @@ class TestOrderedList(unittest.TestCase):
         o_list = make_test_list_rev()
         self.assertEqual(o_list.head.value, 8)
         self.assertEqual(o_list.tail.value, 1)
-        
+
         self.assertEqual(o_list.head.next.value, 7)
         self.assertEqual(o_list.head.next.next.value, 3)
 
         self.assertEqual(o_list.tail.prev.value, 2)
         self.assertEqual(o_list.tail.prev.prev.value, 3)
-
 
     def test_find(self) -> None:
         o_list = make_test_list()
@@ -56,7 +60,17 @@ class TestOrderedList(unittest.TestCase):
             self.assertEqual(o_list.find(i).value, i)
 
         for i in (0, 4, 5, 6):
-           self.assertIsNone(o_list.find(i))
+            self.assertIsNone(o_list.find(i))
+
+        # find in empty list
+        e_list = OrderedList(asc=True)
+        self.assertIsNone(e_list.find(1))
+
+        # find in one element list
+        one_list = OrderedList(asc=True)
+        one_list.add(1)
+        self.assertEqual(one_list.find(1).value, 1)
+        self.assertIsNone(one_list.find(2))
 
     def test_find_rev(self) -> None:
         o_list = make_test_list_rev()
@@ -65,7 +79,17 @@ class TestOrderedList(unittest.TestCase):
             self.assertEqual(o_list.find(i).value, i)
 
         for i in (0, 4, 5, 6):
-           self.assertIsNone(o_list.find(i))
+            self.assertIsNone(o_list.find(i))
+
+        # find in empty list
+        e_list = OrderedStringList(asc=True)
+        self.assertIsNone(e_list.find(1))
+
+        # find in one element list
+        one_list = OrderedStringList(asc=True)
+        one_list.add('a')
+        self.assertEqual(one_list.find('a').value, 'a')
+        self.assertIsNone(one_list.find('b'))
 
     def test_delete(self) -> None:
         o_list = make_test_list()
@@ -79,6 +103,9 @@ class TestOrderedList(unittest.TestCase):
         self.assertIsNone(o_list.head)
         self.assertIsNone(o_list.tail)
 
+        # check deletion from empty list
+        o_list.delete(10)
+
     def test_delete_rev(self) -> None:
         o_list = make_test_list_rev()
         len_ = 5
@@ -91,12 +118,15 @@ class TestOrderedList(unittest.TestCase):
         self.assertIsNone(o_list.head)
         self.assertIsNone(o_list.tail)
 
+        # check deletion from empty list
+        o_list.delete(10)
+
 
     def test_is_ordered_str(self) -> None:
         o_list = make_test_list_str()
         self.assertEqual(o_list.head.value, 'a')
         self.assertEqual(o_list.tail.value, ' z')
-        
+
         self.assertEqual(o_list.head.next.value, 'b ')
         self.assertEqual(o_list.head.next.next.value, 'm')
         self.assertEqual(o_list.tail.prev.value, 'y')
@@ -106,7 +136,7 @@ class TestOrderedList(unittest.TestCase):
         o_list = make_test_list_str_rev()
         self.assertEqual(o_list.head.value, ' z')
         self.assertEqual(o_list.tail.value, 'a')
-        
+
         self.assertEqual(o_list.head.next.value, 'y')
         self.assertEqual(o_list.head.next.next.value, 'm')
         self.assertEqual(o_list.tail.prev.value, 'b ')
@@ -119,7 +149,18 @@ class TestOrderedList(unittest.TestCase):
             self.assertIsNotNone(o_list.find(i))
 
         for i in ('c', 'd', 'q', 't'):
-           self.assertIsNone(o_list.find(i))
+            self.assertIsNone(o_list.find(i))
+
+        # find in empty list
+        e_list = OrderedStringList(asc=True)
+        self.assertIsNone(e_list.find('a'))
+
+        # find in one element list
+        one_list = OrderedStringList(asc=True)
+        one_list.add('a')
+        self.assertEqual(one_list.find('a').value, 'a')
+        self.assertIsNone(one_list.find('b'))
+            
 
     def test_delete_str(self) -> None:
         o_list = make_test_list_str()
@@ -132,6 +173,9 @@ class TestOrderedList(unittest.TestCase):
 
         self.assertIsNone(o_list.head)
         self.assertIsNone(o_list.tail)
+
+        # check deletion from empty list
+        o_list.delete('b')
 
 
 if __name__ == '__main__':
