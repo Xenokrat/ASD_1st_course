0b073adb2c48eb4631490852e6bc84057d8285bd
Mantis <barabanovpv95@gmail.com>
2022-09-11 11:30:28 +0300

    feat 10: add PowerSet data structure

---
+++ power_set.py
@@ -0,0 +1,114 @@
+from typing import NoReturn, TypeVar
+
+S = TypeVar('S', bound="PowerSet")
+
+class PowerSet:
+    def __init__(self) -> None:
+        self.container = {}
+
+    def size(self) -> int:
+        """
+        Returns:
+            int: length of set
+        """
+        return len(self.container)
+
+    def put(self, value: object) -> NoReturn:
+        """Add any value in set, set have no duplicates
+
+        Args:
+            value (_type_): value to add
+        """
+        self.container[value] = None
+
+    def get(self, value: object) -> bool:
+        """Returns True if value in set, False if not
+
+        Args:
+            value (_type_): value to check in set
+
+        Returns:
+            bool: True if value in set, False if not
+        """
+        return (value in self.container)
+
+    def remove(self, value: object) -> bool:
+        """Delete value in set, return True if succeed, False if value
+           not in set
+
+        Args:
+            value (_type_): value to delete
+
+        Returns:
+            bool: True if value deleted, else False
+        """
+        if self.get(value):
+            del self.container[value]
+            return True
+        return False
+
+    def intersection(self, set2: S) -> S:
+        """Return intersection of current set and another set
+
+        Args:
+            set2 (PowerSet): another instance of PowerSet
+
+        Returns:
+            PowerSet: set of elements that is intersection 
+            of current set and another set
+        """
+        new_set = PowerSet()
+        
+        for item in self.container:
+            if set2.get(item):
+                new_set.put(item)
+                
+        return new_set
+
+    def union(self, set2: S) -> S:
+        """Return union of elements between current set and another set
+
+        Args:
+            set2 (PowerSet): another instance of PowerSet
+
+        Returns:
+            PowerSet: PowerSet that is union of elements of current set
+            and another set
+        """
+        new_set = PowerSet()
+        for item in self.container:
+            new_set.put(item)
+        for item in set2.container:
+            new_set.put(item)
+        return new_set
+
+    def difference(self, set2: S) -> S:
+        """Return elements that is in current set but not in another set
+
+        Args:
+            set2 (PowerSet): set to compare
+
+        Returns:
+            PowerSet: difference between current set and another set
+        """
+        new_set = PowerSet()
+        
+        for item in self.container:
+            if not set2.get(item):
+                new_set.put(item)
+                
+        return new_set
+
+    def issubset(self, set2: S) -> bool:
+        """Return True if set2 is a subset of current set, else return False
+
+        Args:
+            set2 (PowerSet): another instance of PowerSet
+            
+        Returns:
+            bool: True if set2 is a subset of current set, else return False
+        """
+        return all(
+            item in self.container 
+            for item in set2.container
+        )
